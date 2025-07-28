5c56dc7148f21915ec8e90e3ea4579f37f327836
Gaurav.S.Srivastava <gauravsrivastava1501@gmail.com>
2025-07-28 23:34:02 +0530

    Persistence Layer and JPA Repository

--- src/main/java/com/learning/springbootWebApplication/controllers/EmployeeController.java
+++ src/main/java/com/learning/springbootWebApplication/controllers/EmployeeController.java
@@ -1,23 +1,37 @@
 package com.learning.springbootWebApplication.controllers;
 
 import com.learning.springbootWebApplication.dto.EmployeeDTO;
+import com.learning.springbootWebApplication.entities.EmployeeEntity;
+import com.learning.springbootWebApplication.repositories.EmployeeRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/employees")
 public class EmployeeController {
 
+    private final EmployeeRepository employeeRepository;
+
+    public EmployeeController(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
     @GetMapping("/{employeeID}")
-    public EmployeeDTO getEmployeeId(@PathVariable Long employeeID){
-        return new EmployeeDTO(22, LocalDate.of(2025, 7, 27), "gaurav@123", employeeID, true,  "gaurav");
+    public EmployeeEntity getEmployeeId(@PathVariable Long employeeID){
+        return employeeRepository.findById(employeeID).orElse(null);
     }
 
     @GetMapping
-    public String getAllEmployees(@RequestParam(required = false, name = "newAge") Integer age, @RequestParam(required = false) String sortBy){
-        return "Hi age is " + age + " " + sortBy;
+    public List<EmployeeEntity> getAllEmployees(@RequestParam(required = false, name = "newAge") Integer age, @RequestParam(required = false) String sortBy){
+        return employeeRepository.findAll();
+    }
+
+    @PostMapping
+    public EmployeeEntity createNewEmployee(@RequestBody EmployeeEntity inputEmployees){
+        return employeeRepository.save(inputEmployees);
     }
 
 }
