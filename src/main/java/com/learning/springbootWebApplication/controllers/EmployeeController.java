d4591cf6fe355d9dd0ded5ed8e923096ef450e17
Gaurav.S.Srivastava <gauravsrivastava1501@gmail.com>
2025-07-28 14:06:37 +0530

    Get mapping in springboot

--- src/main/java/com/learning/springbootWebApplication/controllers/EmployeeController.java
+++ src/main/java/com/learning/springbootWebApplication/controllers/EmployeeController.java
@@ -2,18 +2,22 @@ package com.learning.springbootWebApplication.controllers;
 
 import com.learning.springbootWebApplication.dto.EmployeeDTO;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 
 @RestController
+@RequestMapping(path = "/employees")
 public class EmployeeController {
 
-    @GetMapping("/employees/{employeeID}")
-    public EmployeeDTO getEmployeeId(@PathVariable Long employeeId){
-        return new EmployeeDTO(22, LocalDate.of(2025, 7, 27), "gaurav@123", employeeId, true,  "gaurav");
+    @GetMapping("/{employeeID}")
+    public EmployeeDTO getEmployeeId(@PathVariable Long employeeID){
+        return new EmployeeDTO(22, LocalDate.of(2025, 7, 27), "gaurav@123", employeeID, true,  "gaurav");
+    }
+
+    @GetMapping
+    public String getAllEmployees(@RequestParam(required = false, name = "newAge") Integer age, @RequestParam(required = false) String sortBy){
+        return "Hi age is " + age + " " + sortBy;
     }
 
 }
