f91c719dc1bc6d1922bfe4319551b38208cd3139
Gaurav.S.Srivastava <gauravsrivastava1501@gmail.com>
2025-07-29 19:58:35 +0530

    CRUD features implemented along with proper status code

--- src/main/java/com/learning/springbootWebApplication/controllers/EmployeeController.java
+++ src/main/java/com/learning/springbootWebApplication/controllers/EmployeeController.java
@@ -4,11 +4,16 @@ import com.learning.springbootWebApplication.dto.EmployeeDTO;
 import com.learning.springbootWebApplication.entities.EmployeeEntity;
 import com.learning.springbootWebApplication.repositories.EmployeeRepository;
 import com.learning.springbootWebApplication.services.EmployeeService;
+import com.sun.net.httpserver.HttpServer;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(path = "/employees")
@@ -21,18 +26,37 @@ public class EmployeeController {
     }
 
     @GetMapping("/{employeeID}")
-    public EmployeeDTO getEmployeeById(@PathVariable Long employeeID){
-        return employeeService.getEmployeeById(employeeID);
+    public ResponseEntity<EmployeeDTO> getEmployeeById(@PathVariable Long employeeID){
+        Optional<EmployeeDTO> employeeDTO =  employeeService.getEmployeeById(employeeID);
+        return employeeDTO
+                .map(employeeDTO1 -> ResponseEntity.ok(employeeDTO1))
+                .orElse(ResponseEntity.notFound().build());
     }
 
     @GetMapping
-    public List<EmployeeDTO> getAllEmployees(@RequestParam(required = false, name = "newAge") Integer age, @RequestParam(required = false) String sortBy){
-        return employeeService.getAllEmployees();
+    public ResponseEntity<List<EmployeeDTO> >getAllEmployees(@RequestParam(required = false, name = "newAge") Integer age, @RequestParam(required = false) String sortBy){
+        return ResponseEntity.ok(employeeService.getAllEmployees());
     }
 
     @PostMapping
-    public EmployeeDTO createNewEmployee(@RequestBody EmployeeDTO inputEmployees){
-        return employeeService.createNewEmployee(inputEmployees);
+    public ResponseEntity<EmployeeDTO> createNewEmployee(@RequestBody EmployeeDTO inputEmployees){
+        EmployeeDTO savedEmployeeDTO = employeeService.createNewEmployee(inputEmployees);
+        return new ResponseEntity<>(savedEmployeeDTO, HttpStatus.CREATED);
+    }
+
+    @PutMapping(path = "/{employeeId}")
+    public ResponseEntity<EmployeeDTO> updateEmployeeById(@RequestBody EmployeeDTO employeeDTO, @PathVariable Long employeeId){
+        return ResponseEntity.ok(employeeService.updateEmployeeById(employeeDTO, employeeId));
+    }
+
+    @DeleteMapping(path = "/{employeeId}")
+    public boolean deleteEmployeeById(@PathVariable Long employeeId){
+         return employeeService.deleteEmployeeById(employeeId);
+    }
+
+    @PatchMapping(path = "/{employeeId}")
+    public EmployeeDTO updatePartialEmployeeById(@RequestBody Map<String, Object> updates, @PathVariable Long employeeId){
+        return employeeService.updatePartialEmployeeById(employeeId, updates);
     }
 
 }
--- src/main/java/com/learning/springbootWebApplication/services/EmployeeService.java
+++ src/main/java/com/learning/springbootWebApplication/services/EmployeeService.java
@@ -5,8 +5,12 @@ import com.learning.springbootWebApplication.entities.EmployeeEntity;
 import com.learning.springbootWebApplication.repositories.EmployeeRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ReflectionUtils;
 
+import java.lang.reflect.Field;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -19,10 +23,12 @@ public class EmployeeService {
         this.modelMapper = modelMapper;
     }
 
-    public EmployeeDTO getEmployeeById(Long employeeID) {
-        EmployeeEntity employeeEntity =  employeeRepository.findById(employeeID).orElse(null);
-
-        return modelMapper.map(employeeEntity, EmployeeDTO.class);
+    public Optional<EmployeeDTO> getEmployeeById(Long employeeID) {
+//        EmployeeEntity employeeEntity =  employeeRepository.findById(employeeID).orElse(null);
+//
+//        return modelMapper.map(employeeEntity, EmployeeDTO.class);
+        return employeeRepository.findById(employeeID)
+                .map(employeeEntity -> modelMapper.map(employeeEntity, EmployeeDTO.class));
     }
 
     public List<EmployeeDTO> getAllEmployees() {
@@ -37,5 +43,35 @@ public class EmployeeService {
         EmployeeEntity toSave = modelMapper.map(inputEmployees, EmployeeEntity.class);
         EmployeeEntity savedEmployeeEntity =  employeeRepository.save(toSave);  //Repo can only accepts employee entity so we should send entity or dto
         return modelMapper.map(savedEmployeeEntity, EmployeeDTO.class);
+    }
+
+    public EmployeeDTO updateEmployeeById(EmployeeDTO employeeDTO, Long employeeId) {
+        EmployeeEntity employeeEntity = modelMapper.map(employeeDTO, EmployeeEntity.class);
+        employeeEntity.setId(employeeId);
+        EmployeeEntity savedEmployeeEntity = employeeRepository.save(employeeEntity);
+        return modelMapper.map(savedEmployeeEntity, EmployeeDTO.class);
+    }
+
+    public boolean isExistsByEmployeeId(Long employeeId){
+        return employeeRepository.existsById(employeeId);
+    }
+
+    public boolean deleteEmployeeById(Long employeeId) {
+        boolean exists = isExistsByEmployeeId(employeeId);
+        if(!exists) return false;
+        employeeRepository.deleteById(employeeId);
+        return true;
+    }
+
+    public EmployeeDTO updatePartialEmployeeById(Long employeeId, Map<String, Object> updates) {
+        boolean exists = isExistsByEmployeeId(employeeId);
+        if(!exists) return null;
+        EmployeeEntity employeeEntity = employeeRepository.findById(employeeId).get();
+        updates.forEach((field, value) ->{
+            Field fieldToBeUpdated = ReflectionUtils.findField(EmployeeEntity.class, field);
+            fieldToBeUpdated.setAccessible(true);
+            ReflectionUtils.setField(fieldToBeUpdated, employeeEntity, value);
+        });
+        return modelMapper.map(employeeRepository.save(employeeEntity), EmployeeDTO.class);
     }
 }
